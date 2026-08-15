79ae1826369c092cb1377c6cdb9c5eb9c31045d8
HaoH0580 <HaoH0580@users.noreply.github.com>
2026-08-15 21:44:13 +0800

    Add calendar and weakness progress dashboard

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'training-center-v11';
-const FILES = ['./','./index.html','./styles.css?v=10','./questions.js?v=10','./app.js?v=10','./manifest.webmanifest'];
+const CACHE = 'training-center-v12';
+const FILES = ['./','./index.html','./styles.css?v=11','./questions.js?v=11','./app.js?v=11','./manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
